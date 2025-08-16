6992fe9a6b85346dcb854bc402155859399cab20
Pranaya Ghimire <pranayaghimire88083@gmail.com>
2025-08-16 23:29:13 +0545

    Minor Fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='font-bold bg-gradient-to-b from-teal-600 to-teal-800 text-white p-4 text-center'>
-        &copy;@taskmanagerapp | All Rights Reserved
+        &copy;@taskmanagementapp | All Rights Reserved
     </div>
   )
 }
